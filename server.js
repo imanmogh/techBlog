c42ccd77367bd7802c196206c0d0303968ea8c28
Iman Moghaddas <imanmoghaddas@yahoo.com>
2023-06-08 02:04:56 -0400

    Added const PORT = process.env.PORT || 3001 to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+require('dotenv').config();
 
 // Helper function
 const helpers = require('./utils/helpers');
